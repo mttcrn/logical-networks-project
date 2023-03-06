713770e826a27662d8f6b41bb028f6016832da2c
Caterina Motti <94934527+mttcrn@users.noreply.github.com>
2023-03-06 08:36:10 +0100

    Output error fix

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -24,13 +24,13 @@ end project_reti_logiche;
 
 architecture Behavioral of project_reti_logiche is
     type state_type is (RST, WAIT_START, READ, ELAB, PRINT);
+    signal s_curr, s_next: state_type;
     signal o_z0_next, o_z1_next, o_z2_next, o_z3_next : std_logic_vector(7 downto 0);
     signal o_done_next : std_logic;
-    signal s_curr, s_next: state_type;
-    signal o_addr: std_logic_vector(0 to 15);
-    signal count : integer range 0 to 3 ;
+    signal o_addr, o_addr_true: std_logic_vector(15 downto 0); 
     signal o_select : std_logic_vector(0 to 1);
-    
+    signal count : integer range 0 to 15;
+    signal dato : integer;
     
 begin    
 
@@ -51,7 +51,7 @@ begin
                 o_z1 <= o_z1_next;
                 o_z2 <= o_z2_next;
                 o_z3 <= o_z3_next;
-            else 
+            else
                 o_z0 <= "00000000";
                 o_z1 <= "00000000";
                 o_z2 <= "00000000";
@@ -59,7 +59,7 @@ begin
             end if;
     end process;
     
-   TL : process(i_rst, i_start, o_done_next)
+   TL : process(i_rst, i_start, i_mem_data)
         begin 
             case s_curr is
                 when RST => 
@@ -81,17 +81,18 @@ begin
                         s_next <= ELAB; 
                     end if;
                 when ELAB =>
-                    if(o_done_next='0') then
-                        s_next <= ELAB;
-                    elsif(o_done_next='1') then
+                    if (i_mem_data'EVENT) then
                         s_next <= PRINT;
+                    else 
+                        s_next <= ELAB;  
                     end if;
                 when PRINT =>
-                        s_next <= WAIT_START;
+                    s_next <= WAIT_START;
                 end case;  
-        end process TL;
+        end process;
         
   R : process(s_curr, i_start, i_w, i_mem_data)
+    -- se rimuovo i_w dalla sensitivity list non mi esce XX in output, ma nemmeno l'output corretto
     begin
         case s_curr is
             when RST =>
@@ -101,47 +102,58 @@ begin
                 o_z2_next <= (Others => '0');
                 o_z3_next <= (Others => '0');
                 o_done_next <= '0';
+                o_mem_we <= '0';
+                o_mem_en <= '0';
             when WAIT_START =>
-                if (i_start='1') then
                     count <= 0;
-                end if;
+                    o_mem_we <= '0';
+                    o_mem_en <= '0';
+                    o_done_next <= '0';
+                    o_addr <= (Others => '0');
             when READ =>
                 if(i_start='1') then
-                    if(count<o_select'LENGTH) then
+                    if(count < o_select'LENGTH) then
                         o_select(count) <= i_w;
-                    else
+                    elsif (count >= o_select'LENGTH) then
                         o_addr(count-o_select'LENGTH) <= i_w;
                     end if;  
-                    count <= count + 1;
+                    count <= count + 1; --# of bits
                 end if;
             when ELAB =>
-                if(o_done_next='0') then
-                    --for i in 0 to 15 loop
-                      --  if(i<count+1) then
-                            --o_mem_addr <= ((15-i) => o_addr(count-i));
-                        --    o_mem_addr(15-i)<=o_addr(count-i);
-                       -- end if;
-                    -- end loop;
-                    o_mem_addr <= (o_mem_addr'LENGTH-1  downto count+1 => '0') & o_addr(count downto 0);
-                    o_mem_we <= '0';
+                   for i in 0 to o_mem_addr'LENGTH-1 loop
+                       if(i < count - o_select'LENGTH) then
+                            o_mem_addr(i) <= o_addr(count - o_select'LENGTH - 1 - i);
+                       elsif(i >= count - o_select'LENGTH) then
+                            o_mem_addr(i) <= '0';
+                       end if;
+                    end loop;
                     o_mem_en <= '1';
-                           --Modulo Memoria
-                    o_done_next <= '1';              
-                end if;
+                    o_mem_we <= '0';         
             when PRINT =>
-                o_done_next <= '0';
-                case o_select is
-                    when "00" =>
-                        o_z0_next <= i_mem_data;
-                    when "01" =>
-                        o_z1_next <= i_mem_data;
-                    when "10" =>
-                        o_z2_next <= i_mem_data;
-                    when "11" =>
-                        o_z3_next <= i_mem_data; 
-                    when others =>
-                end case;
-        end case;
-    end process R;
-
+                o_done_next <= '1';
+                o_mem_en <= '0';
+                if (o_select = "00") then
+                    o_z0_next <= i_mem_data;
+                    o_z1_next <= (Others => '0');
+                    o_z2_next <= (Others => '0');
+                    o_z3_next <= (Others => '0');
+                elsif (o_select = "01") then
+                    o_z1_next <= i_mem_data;
+                    o_z0_next <= (Others => '0');
+                    o_z2_next <= (Others => '0');
+                    o_z3_next <= (Others => '0');
+                elsif (o_select = "10") then
+                    o_z2_next <= i_mem_data;
+                    o_z1_next <= (Others => '0');
+                    o_z0_next <= (Others => '0');
+                    o_z3_next <= (Others => '0');
+                elsif (o_select = "11") then
+                    o_z1_next <= (Others => '0');
+                    o_z2_next <= (Others => '0');
+                    o_z0_next <= (Others => '0');
+                    o_z3_next <= i_mem_data;
+                end if;
+            end case;
+    end process;
+    
 end Behavioral;
