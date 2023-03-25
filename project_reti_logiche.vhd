d6ebb77ce8651c804eb8a7c36a4ef0ef5d7ee955
Caterina Motti <94934527+mttcrn@users.noreply.github.com>
2023-03-25 21:25:03 +0100

    Commit finale

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -1,3 +1,10 @@
+--
+-- Prova finale di Reti Logiche
+-- 2022/2023
+-- 
+-- Andrea Grassi 10741092
+-- Caterina Motti 10717568
+--
 library IEEE;
     use IEEE.STD_LOGIC_1164.ALL;
     use IEEE.NUMERIC_STD.ALL;
@@ -23,6 +30,7 @@ entity project_reti_logiche is
 end project_reti_logiche;
 
 architecture Behavioral of project_reti_logiche is
+    --enumerazione degli stati della FSM
     type state_type is (WAIT_START, READ, WAIT_MEM, ELAB, PRINT);
     signal s_curr: state_type;
     signal o_z0_next, o_z1_next, o_z2_next, o_z3_next : std_logic_vector(7 downto 0);
@@ -34,6 +42,7 @@ begin
 
     process(i_clk,i_rst)
     begin
+        --reset asincrono
         if(i_rst='1') then
             o_done<='0';
             o_z0<=(others=>'0');
@@ -46,6 +55,7 @@ begin
             o_z3_next<=(others=>'0');
             s_curr<=WAIT_START;
             
+        --funzionamento sincrono della FSM
         elsif(rising_edge(i_clk)) then
             case s_curr is
                 when WAIT_START =>
@@ -55,8 +65,10 @@ begin
                     o_z2<=(others=>'0');
                     o_z3<=(others=>'0');
                     if(i_start='1') then
+                        --memorizzazione del primo bit di s_select (più significativo)
                         s_select(1)<=i_w;
                         flag<='0';
+                        --inizializzazione di s_addr che garantisce l'estensione con '0' sui bit più significativi
                         s_addr<=(others=>'0');
                         s_curr<=READ;
                     else 
@@ -64,16 +76,21 @@ begin
                     end if;  
                                       
                 when READ =>
+                    --lettura dell'input i_w
                     if(i_start='1') then
                         if(flag = '0') then
+                            --memorizzazione del secondo bit di s_select (meno significativo)
                             s_select(0)<=i_w;
                             flag <= '1';
                         else
+                            --effettuo lo shift dell'intero indirizzo a sinistra
                             s_addr(15 downto 1)<=s_addr(14 downto 0);
+                            --assegno al bit meno significativo i_w
                             s_addr(0)<=i_w;
                         end if;
                         s_curr<=READ;
                     else
+                        --predisposizione input della memoria
                         o_mem_addr<=s_addr;
                         o_mem_we<='0';
                         o_mem_en<='1';
@@ -81,11 +98,13 @@ begin
                     end if;
                     
                 when WAIT_MEM =>
+                    --stato che garantisce un ciclo di clock per poter prendere in output il contenuto della memoria
                     o_mem_en<='0';
                     o_mem_we<='0';
                     s_curr<=ELAB;
                     
                 when ELAB =>
+                    --selezione dell'uscita su cui mostrare il risultato della richiesta di lettura alla memoria
                     case s_select is
                         when "00" => o_z0_next <= i_mem_data;
                         when "01" => o_z1_next <= i_mem_data;
